b9a179aa4775c5acfd8a9a565b9e6b21d964c1ec
SeonHyeonWook <seonhyunuk@gmail.com>
2024-10-05 16:51:32 +0900

    api 수정

--- src/api/api.js
+++ src/api/api.js
@@ -1,11 +1,4 @@
-// api.js
 import axios from "axios";
-
-// JWT 토큰을 가져오는 함수
-const getJwtToken = () => {
-  // 로컬 스토리지에서 JWT 토큰을 가져옴
-  return localStorage.getItem("jwtToken");
-};
 
 // Axios 인스턴스 생성
 const api = axios.create({
@@ -14,20 +7,5 @@ const api = axios.create({
     "Content-Type": "application/json",
   },
 });
-
-// 요청 인터셉터 설정 (모든 요청에 JWT 토큰 추가)
-api.interceptors.request.use(
-  (config) => {
-    const token = getJwtToken();
-    if (token) {
-      // Authorization 헤더에 토큰을 추가
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
-);
 
 export default api;
